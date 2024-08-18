71888b45a14998bce6ec8675630bb567dcf43470
Dax Davis <daxantz@hotmail.com>
2024-08-18 16:38:08 -0400

    cahnged hardcoded values to use data passed into component

--- src/components/ListingItem.jsx
+++ src/components/ListingItem.jsx
@@ -4,15 +4,15 @@ export default function ListingItem({ itemData }) {
       <div className="flex justify-center">
         <img
           className="w-full max-w-64 rounded-2xl"
-          src="https://i.ebayimg.com/images/g/4dgAAOSwMHdhqysw/s-l1200.jpg"
+          src={itemData.image}
           alt="item image"
         />
       </div>
       <div className="flex flex-row justify-between px-5 mt-10">
         <div>
-          <p className="font-bold">Item Name</p>
-          <p className="text-stone-400 text-xl">Item Category</p>
-          <p>Item Price</p>
+          <p className="font-bold">{itemData.title}</p>
+          <p className="text-stone-400 text-xl">{itemData.category}</p>
+          <p>{itemData.price}</p>
         </div>
         <div className="flex items-end text-white">
           <button className="bg-blue-500 py-3 px-5 rounded-full">
