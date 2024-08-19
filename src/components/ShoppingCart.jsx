d81b6d010dcc5b6e520189afeafd3cb2d254ed21
Dax Davis <daxantz@hotmail.com>
2024-08-19 16:21:15 -0400

    added relative positioning to parent div so cart count moves with it when resizing screen

--- src/components/ShoppingCart.jsx
+++ src/components/ShoppingCart.jsx
@@ -3,12 +3,17 @@ import { useCartContext } from "../hooks/Cartprovider";
 export default function ShoppingCart({ onClick }) {
   const { cart, setCart } = useCartContext();
   return (
-    <div data-testid="shopping-cart" onClick={onClick} role="button">
+    <div
+      data-testid="shopping-cart"
+      onClick={onClick}
+      role="button"
+      className="relative"
+    >
       <ShoppingCartIcon className="w-8" />
       {cart.length >= 1 && (
         <span
           data-testid="item-count"
-          className="absolute top-3 right-7 bg-red-600 w-6 h-6  rounded-full  text-white text-base text-center"
+          className="absolute bottom-5 left-6 bg-red-600 w-6 h-6  rounded-full  text-white text-base text-center"
         >
           {cart.length}
         </span>
