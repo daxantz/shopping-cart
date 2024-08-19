8a6a31aea6feab435a621b3e90b0d2cb0bb63b72
Dax Davis <daxantz@hotmail.com>
2024-08-19 14:03:37 -0400

    itempage now renders information for whatever item is currently selected

--- src/components/Itempage.jsx
+++ src/components/Itempage.jsx
@@ -1,3 +1,35 @@
+import { useEffect, useState } from "react";
+import { useFetcher, useLoaderData, useParams } from "react-router-dom";
+
 export default function Listingpage() {
-  return <h1>Item page</h1>;
+  const itemData = useLoaderData();
+  const { id } = useParams();
+  const [selectedItem, setSelectedItem] = useState();
+
+  useEffect(() => {
+    setSelectedItem(itemData.items.find((item) => item.id === Number(id)));
+    console.log("id changed, now switching to new item", selectedItem);
+  }, [id]);
+  if (!selectedItem) return <p>loading...</p>;
+  return (
+    <div>
+      <div className="max-w-28">
+        <img src={selectedItem.image} alt="Item image" />
+      </div>
+      <div>
+        <h2>{selectedItem.title}</h2>
+        <p>{selectedItem.category}</p>
+        <p>{selectedItem.price}</p>
+        <div>
+          <button>-</button>
+          <span>{selectedItem.quantity}</span>
+          <button>+</button>
+        </div>
+        <div>
+          <p>item description</p>
+          <button>Add to Cart</button>
+        </div>
+      </div>
+    </div>
+  );
 }
