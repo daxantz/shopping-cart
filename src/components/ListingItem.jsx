9b100bd6656623d43852dc27b1299862c6f1b0da
Dax Davis <daxantz@hotmail.com>
2024-08-19 12:53:58 -0400

    changed styles for better ui

--- src/components/ListingItem.jsx
+++ src/components/ListingItem.jsx
@@ -1,20 +1,21 @@
 export default function ListingItem({ itemData }) {
   return (
-    <div className="text-2xl border-2 border-black rounded-2xl flex flex-col py-6  w-3/12">
-      <div className="flex justify-center">
+    <div className=" border-2 border-black rounded-2xl flex flex-col py-6 justify-around h-96 max-w-full ">
+      <div className="flex justify-center  overflow-hidden w-36 mx-auto">
         <img
-          className="w-full max-w-64 rounded-2xl"
+          className="w-full object-cover"
           src={itemData.image}
           alt="item image"
         />
       </div>
-      <div className="flex flex-row justify-between px-5 mt-10">
-        <div>
+      <div className="px-5 flex flex-col gap-5">
+        <div className="">
           <p className="font-bold">{itemData.title}</p>
           <p className="text-stone-400 text-xl">{itemData.category}</p>
           <p>{itemData.price}</p>
         </div>
-        <div className="flex items-end text-white">
+
+        <div className="flex items-end text-white w-13 text-xs">
           <button className="bg-blue-500 py-3 px-5 rounded-full">
             Add to cart
           </button>
