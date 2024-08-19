696fc5a03404b78ada3b56053dd42bd876effb94
Dax Davis <daxantz@hotmail.com>
2024-08-19 12:53:34 -0400

    now renders shop items in a grid format

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,3 +1,16 @@
-export default function Shop(){
-    return <div>Hello from shop</div>
+import { useLoaderData } from "react-router-dom";
+import ListingItem from "./ListingItem";
+
+export default function Shop() {
+  const itemData = useLoaderData();
+  return (
+    <div className="p-10">
+      <p>All items({itemData.items.length})</p>
+      <div className="grid grid-rows-5 grid-cols-4 gap-5 ">
+        {itemData.items.map((item) => (
+          <ListingItem key={item.id} itemData={item} />
+        ))}
+      </div>
+    </div>
+  );
 }
